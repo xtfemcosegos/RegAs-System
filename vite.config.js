f01b9261698e397fda4cc10ad1371ded17536991
xtfemcosegos <segos911@gmail.com>
2025-12-04 16:18:18 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANTE: Rutas relativas para que funcione en cualquier repositorio de GitHub Pages
-  base: './', 
+  // CAMBIO: Usamos la ruta absoluta del repositorio en lugar de relativa.
+  // Esto hace que las rutas de imágenes y scripts sean infalibles en GitHub Pages.
+  base: '/RegAs-System/', 
 })
